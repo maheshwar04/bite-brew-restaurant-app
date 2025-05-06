7e6be4772c3033c251696c7209c91029e8efeed5
Maheshwar <maheshwarnag04@gmail.com>
2025-05-06 15:16:51 +0530

    added eureka

---
+++ erureka-server/src/main/java/com/bite_brew/erureka_server/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.bite_brew.erureka_server;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(ErurekaServerApplication.class);
+	}
+
+}
---
+++ erureka-server/src/test/java/com/bite_brew/erureka_server/ErurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bite_brew.erureka_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ErurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
